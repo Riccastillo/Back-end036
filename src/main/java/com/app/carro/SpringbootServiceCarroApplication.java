ef9f755e44f94a6f7893946595c3d4b16d45afcb
Ricardo <ricardocb007@gmail.com>
2023-11-01 12:43:29 -0600

    Se agregaron configuraciones de cliente eureka

--- src/main/java/com/app/carro/SpringbootServiceCarroApplication.java
+++ src/main/java/com/app/carro/SpringbootServiceCarroApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.app.carro.entity.Carro;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootServiceCarroApplication {
 
 	public static void main(String[] args) {
