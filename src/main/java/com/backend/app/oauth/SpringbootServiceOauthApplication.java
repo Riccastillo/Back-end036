64188600d7d8b63b7b44ca5086a2a46fab2c106d
Ricardo <ricardocb007@gmail.com>
2023-11-29 16:54:58 -0600

    Video Creando Servicio OAuth

---
+++ src/main/java/com/backend/app/oauth/SpringbootServiceOauthApplication.java
@@ -0,0 +1,15 @@
+package com.backend.app.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringbootServiceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/app/oauth/SpringbootServiceOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.app.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
