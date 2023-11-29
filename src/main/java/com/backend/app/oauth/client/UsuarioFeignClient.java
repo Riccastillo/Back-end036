547b854efe3ba49f724e821f15cbe907c810633e
Ricardo <ricardocb007@gmail.com>
2023-11-29 17:28:11 -0600

    video Creando Interfaz Feign

---
+++ src/main/java/com/backend/app/oauth/client/UsuarioFeignClient.java
@@ -0,0 +1,13 @@
+package com.backend.app.oauth.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.backend.app.commons.users.entity.Usuario;
+@FeignClient("servicio-usuarios")
+public interface UsuarioFeignClient {
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String username);
+
+}
