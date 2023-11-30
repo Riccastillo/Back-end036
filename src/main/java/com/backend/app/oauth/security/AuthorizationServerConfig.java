50ca70467bbbb4141aa7f11be649c15708080ed7
Ricardo <ricardocb007@gmail.com>
2023-11-30 01:33:10 -0600

    video Configurando Clientes

--- src/main/java/com/backend/app/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/backend/app/oauth/security/AuthorizationServerConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -16,6 +19,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Override    //endpoint /oauth/token  es de tipo POST 
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -25,6 +31,32 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		.accessTokenConverter(jwtAccessTokenConverter());
 	}
 	
+	
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+
+		security.tokenKeyAccess("permitAll()")
+		.checkTokenAccess("isAuthenticated()");
+	}
+
+
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		
+		clients.inMemory().withClient("backEndApp")
+		.secret(passwordEncoder.encode("contra123"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+		
+		
+	}
+
+
+
 	@Bean
 	public JwtTokenStore jwtTokenStore() {
 		return new JwtTokenStore(jwtAccessTokenConverter());
